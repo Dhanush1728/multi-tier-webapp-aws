c5edd98c34a6dd92797ee8a5596ef2c1b89d939e
Dhanush <YOUR_GITHUB_EMAIL>
2026-02-24 15:21:47 +0000

    Initial backend structure

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: "multi-tier-db.cxo662iosvk3.eu-north-1.rds.amazonaws.com",
+  user: "admin",
+  password: "dhanush1728",
+  database: "myapp"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to database");
+  }
+});
+
+app.post("/add-user", (req, res) => {
+  const { name, email } = req.body;
+
+  db.query(
+    "INSERT INTO users (name, email) VALUES (?, ?)",
+    [name, email],
+    (err, result) => {
+      if (err) return res.status(500).json(err);
+      res.json({ message: "User added successfully" });
+    }
+  );
+});
+
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, results) => {
+    if (err) return res.status(500).json(err);
+    res.json(results);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
